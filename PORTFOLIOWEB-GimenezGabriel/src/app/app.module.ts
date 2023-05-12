12e349ae520bb6b77a8d3874dcc6c782406658be
GabrielGim <Gabrielgimenezz2000@gmail.com>
2023-05-12 16:43:14 -0300

    Creacion del header con redes

--- PORTFOLIOWEB-GimenezGabriel/src/app/app.module.ts
+++ PORTFOLIOWEB-GimenezGabriel/src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./AppComponent";
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+
   ],
   imports: [
     BrowserModule,
